3b9eb4503b7330fb67e7f0e16e722fefd51105b0
waenkit0076 <rudresh.r.met23@itbhu.ac.in>
2026-08-13 00:03:58 +0530

    testing

--- src/Detection.py
+++ src/Detection.py
@@ -6,7 +6,7 @@ class DetectionEngine:
 
     def detect(self, LogEvent):
         # Example detection logic
-        if "-encodedcommand" in LogEvent.command_line.lower():
+        if "-EncodedCommand" in LogEvent.command_line():
             alert = Alerts(
                 title="Suspicious PowerShell Command Detected",
                 severity="High",
--- test/test_detection_rules.py
+++ test/test_detection_rules.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from src.models import LogEvent, Alerts
 from src.Parser import SysmonParser
 from src.Detection import DetectionEngine
@@ -7,5 +12,5 @@ logs=parser.load("data/sample.json")
 events=parser.parse_logs(logs)
 
 det=DetectionEngine()
-Alert=det.detect(events)
+Alert=det.detect(events[0])
 print(Alert)
