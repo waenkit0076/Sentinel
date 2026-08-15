06a95c5a58f3e3539e0397751ab4ee08a311e162
waenkit0076 <rudresh.r.met23@itbhu.ac.in>
2026-08-15 11:48:16 +0530

    improving detection engine

--- src/Detection.py
+++ src/Detection.py
@@ -14,18 +14,22 @@ class DetectionEngine:
                 mitre_technique="T1059.001",
                 confidence=70
             )
-            self.alerts.append(alert)
-        if "powershell.exe" and "invoke-webrequest" or "iwr" in LogEvent.commandline.lower():
+        
+        elif "powershell.exe" and ("invoke-webrequest" or "iwr") in LogEvent.command_line.lower():
             alert=Alerts(
                 title="Suspicious File Download Attempt Detected",
                 severity="High",
                 description=f"Detected a suspicious file download attempt using Invoke-WebRequest Command: {LogEvent.command_line}",
-                mitre_technique="T1105"
+                mitre_technique="T1105",
                 confidence=80
             )
-            self.alerts.append(alert)
-        if "certutil.exe" and ".exe" or ".dll" in LogEvent.command_line.lower():
+        elif "certutil.exe" and (".exe" or ".dll") in LogEvent.command_line.lower():
             alert=Alerts(
-                title=""                
+                title="Susipcious File Download Attempt Detected",
+                severity="High",
+                description=f"Detected a suspicious file downloaded using certutil.exe: {LogEvent.command_line}",
+                mitre_technique="T1105",
+                confidence=70                
             )
+            self.alerts.append()
         return self.alerts
