7f04c0222455a4cbd736b34acadcfb26804f1218
waenkit0076 <rudresh.r.met23@itbhu.ac.in>
2026-08-15 12:14:35 +0530

    improvement

--- src/Detection.py
+++ src/Detection.py
@@ -3,33 +3,42 @@ from src.models import LogEvent, Alerts
 class DetectionEngine:
     def __init__(self):
         self.alerts = []
-
     def detect(self, LogEvent):
-        # Example detection logic
-        if "-EncodedCommand" in LogEvent.command_line:
+        command = LogEvent.command_line.lower()
+
+        if "powershell.exe" in command and "-encodedcommand" in command:
+             alert = Alerts(
+            title="Suspicious PowerShell Command Detected",
+            severity="High",
+            description=f"Detected a suspicious obfuscated PowerShell command: {LogEvent.command_line}",
+            mitre_technique="T1059.001",
+            confidence=70
+            )
+
+        elif "powershell.exe" in command and (
+             "invoke-webrequest" in command or "iwr" in command
+            ):
+             alert = Alerts(
+            title="Suspicious File Download Attempt Detected",
+            severity="High",
+            description=f"Detected a suspicious file download attempt using PowerShell: {LogEvent.command_line}",
+            mitre_technique="T1105",
+            confidence=80
+            )
+
+        elif "certutil.exe" in command and (
+            ".exe" in command or ".dll" in command
+             ):
             alert = Alerts(
-                title="Suspicious PowerShell Command Detected",
-                severity="High",
-                description=f"Detected a suspicious Obfuscated PowerShell command: {LogEvent.command_line}",
-                mitre_technique="T1059.001",
-                confidence=70
+            title="Suspicious File Download Attempt Detected",
+            severity="High",
+            description=f"Detected a suspicious file downloaded using certutil.exe: {LogEvent.command_line}",
+            mitre_technique="T1105",
+            confidence=70
             )
-        
-        elif "powershell.exe" and ("invoke-webrequest" or "iwr") in LogEvent.command_line.lower():
-            alert=Alerts(
-                title="Suspicious File Download Attempt Detected",
-                severity="High",
-                description=f"Detected a suspicious file download attempt using Invoke-WebRequest Command: {LogEvent.command_line}",
-                mitre_technique="T1105",
-                confidence=80
-            )
-        elif "certutil.exe" and (".exe" or ".dll") in LogEvent.command_line.lower():
-            alert=Alerts(
-                title="Susipcious File Download Attempt Detected",
-                severity="High",
-                description=f"Detected a suspicious file downloaded using certutil.exe: {LogEvent.command_line}",
-                mitre_technique="T1105",
-                confidence=70                
-            )
-            self.alerts.append()
-        return self.alerts
+
+        else:
+            return None
+
+        self.alerts.append(alert)
+        return alert
