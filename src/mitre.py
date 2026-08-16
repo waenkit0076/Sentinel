db112253a2d94e2629b6f72258f83f34d15167fd
waenkit0076 <rudresh.r.met23@itbhu.ac.in>
2026-08-16 13:26:16 +0530

    Completed the mitre mapping skeleton.. need to upload data

--- src/mitre.py
+++ src/mitre.py
@@ -1,22 +1,32 @@
 from src.models import MITRE
+from src.Detection import DetectionEngine
 
 MITRE_Techniques={
-    "T1059_1":MITRE(
+    "T1059.001":MITRE(
         id = "T1059.001",
         technique = "Command and Scripting Interpreter: Powershell",
-        tactic = "Execution",
+        tactic = "Execution"
     ),
     
-    "T1024_2":MITRE(
+    "T1024.002":MITRE(
         id="T1024.002",
         technique= "User Execution: Malicious File",
         tactic= "Execution"
     ),
     
-    "T1024_3":MITRE(
+    "T1024.003":MITRE(
         id="T1204.003",
         technique="User Execution: Malicious Image",
         tactic="Execution"
     ),
 }
-print(MITRE_Techniques["T1059_1"])
+
+def mitre(alert):
+    try:
+        x=MITRE_Techniques[alert.mitre_technique]
+    except KeyError:
+        x=None
+    return x
+
+        
+    
--- test/test_detection_rules.py
+++ test/test_detection_rules.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.models import LogEvent, Alerts
 from src.Parser import SysmonParser
 from src.Detection import DetectionEngine
+from src.mitre import mitre
 
 parser=SysmonParser()
 logs=parser.load("data/sample.json")
@@ -14,5 +15,14 @@ events=parser.parse_logs(logs)
 det=DetectionEngine()
 for event in events:
     Alert=det.detect(event)
-    print(Alert)
+    if Alert!=None:
+        Mitre=mitre(Alert)
+        if Mitre !=None:
+            print(Mitre)
+        else:
+            print("MITRE Mapping Unavailable")
+    else:
+        print("Not an Issue")
+        
+    
 
